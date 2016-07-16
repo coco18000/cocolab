2dd17dbbc76d65d5c493e44fd380202f64274c16
Katsuya Kikuchi <kikuchi.k.0711@gmail.com>
2016-07-16 17:58:04 +0900

    update index

--- js/aquarium.js
+++ js/aquarium.js
@@ -2,37 +2,45 @@ window.addEventListener('load',init) ;
 
 //------------------------------------------------//
 //------------------------------------------------//
-var WIDTH = 450;
+var WIDTH = 900;
 var HEIGTH = 300;
 
 var canvas;
 var ctx;
 
 var fishes = [];
+var MAX_FISH = 25;
 //------------------------------------------------//
 //------------------------------------------------//
 
-function fish(x,y,size,speed,direct){
+function fish(x,y,size,speed,direct,rare){
 	this.x = x;
 	this.y = y;
 	this.size = size;
 	this.direct = direct;
 	this.speed = speed;
+	this.rare = rare;
 }
 fish.prototype.Draw = 	function (){
 	ctx.lineWidth = 1.5;
 	ctx.fillRect(this.x+(this.direct*this.size*2/3),this.y-(this.size/10),2,2);
 	ctx.beginPath();
+	if(this.rare){ctx.strokeStyle = 'rgb(230, 180, 34)';}
+	else{ctx.strokeStyle = 'rgb(0, 0, 0)';}
 	ctx.moveTo(this.x+this.direct*this.size,this.y);
 	ctx.quadraticCurveTo(this.x,this.y+this.size,this.x-this.direct*this.size,this.y);
 	ctx.stroke();
 
 	ctx.beginPath();
+	if(this.rare){ctx.strokeStyle = 'rgb(230, 180, 34)';}
+	else{ctx.strokeStyle = 'rgb(0, 0, 0)';}
 	ctx.moveTo(this.x-this.size,this.y);
 	ctx.quadraticCurveTo(this.x,this.y-this.size,this.x+this.size,this.y);
 	ctx.stroke();
 
 	ctx.beginPath();
+	if(this.rare){ctx.strokeStyle = 'rgb(230, 180, 34)';}
+	else{ctx.strokeStyle = 'rgb(0, 0, 0)';}
 	ctx.moveTo(this.x-this.direct*this.size,this.y);
 	ctx.lineTo(this.x-this.direct*this.size-(this.direct*this.size/5),this.y-(this.size/5));
 	ctx.lineTo(this.x-this.direct*this.size-(this.direct*this.size/5),this.y+(this.size/5));
@@ -64,11 +72,11 @@ function init() {
 	canvas.width = WIDTH;
 	canvas.height = HEIGTH;
 
-	var fish_num = Math.floor(Math.random()*9)+1;
+	var fish_num = Math.floor(Math.random()*(MAX_FISH-1))+1;
 	for(var i=0;i<fish_num;i++){
 		var x = Math.floor(Math.random()*(WIDTH-140))+70;
 		var y = Math.floor(Math.random()*(HEIGTH-40))+20;
-		var size  = Math.floor(Math.random()*30)+5;
+		var size  = Math.floor(Math.random()*35)+5;
 		var speed = Math.random()+0.05;
 
 		var direct = Math.random();
@@ -78,7 +86,9 @@ function init() {
 			direct = 1;
 		}
 
-		var new_fish = new fish(x,y,size,speed,direct);
+		var rare = false;
+		if(Math.random()<0.0001) rare = true;
+		var new_fish = new fish(x,y,size,speed,direct,rare);
 		fishes.push(new_fish);
 	}
 	requestAnimationFrame(update);
